0c85cbb9aea0521930c59548dbfdfb6e6a122c6f
haso <haso@ubuntu.(none)>
2014-11-19 16:52:18 +0100

    now broadcast the correct transform so that the groundplane is parallel to the world plane

--- src/floor_detection.cpp
+++ src/floor_detection.cpp
@@ -18,30 +18,15 @@ public:
     floorDetection() {
         _pcl_sub = _nh.subscribe("/filepub/pcl", 1, &floorDetection::detectFloor, this);
         _floor_pub = _nh.advertise<sensor_msgs::PointCloud2>("/floor_detection/floor", 1);
-        _all_pub = _nh.advertise<sensor_msgs::PointCloud2>("/floor_detection/all", 1);
-        transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
     }
 
     void detectFloor(const sensor_msgs::PointCloud2ConstPtr& pclMsg) {
-//        double r, p, y;
-//        _nh.getParam("/floorDetection/r", r);
-//        _nh.getParam("/floorDetection/p", p);
-//        _nh.getParam("/floorDetection/y", y);
-        q.setRPY(-1.9, 0, 0);
-        transform.setRotation(q);
-        _tb.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "floor"));
-
-//        Cloud::Ptr input(new Cloud);
-//        if(pcl::io::loadPCDFile("/home/haso/catkin_ws/src/floor_detection/test_clouds/" + file, *input) == -1) {
-//            std::cout << "File not fond" << std::endl;
-//            return;
-//        }
-
         Cloud::Ptr input(new Cloud);
         Cloud2 cloud2In;
         pcl_conversions::toPCL(*pclMsg, cloud2In);
         pcl::fromPCLPointCloud2(cloud2In, *input);
 
+        //coefficients on form ax + by + cz + d = 0
         pcl::ModelCoefficients coefficients;
         pcl::PointIndices inliers;
         pcl::SACSegmentation<Point> seg;
@@ -51,32 +36,37 @@ public:
         seg.setInputCloud(input);
         seg.segment(inliers, coefficients);
 
+        tf::Vector3 floorNormal(coefficients.values[0], coefficients.values[1], coefficients.values[2]);
+        floorNormal.normalize();
+        double angle = std::acos(floorNormal.z());
+
+        //Broadcast the tranform
+        transform.setOrigin(tf::Vector3(0.0, 0.0, coefficients.values[3]));
+        q.setRPY(-angle, 0, 0);
+        transform.setRotation(q);
+        _tb.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "floor"));
+
+        //Publish pointcloud of floor, not really necessary other than for debugging
         Cloud result(*input, inliers.indices);
         for(Cloud::iterator it = result.begin(); it != result.end(); ++it) {
             it->r = 0;
             it->g = 255;
             it->b = 0;
         }
-        std::cout << "floor plane: " << result.size() << std::endl;
+
         sensor_msgs::PointCloud2 msgOut;
         result.header.frame_id = "floor";
         pcl::toROSMsg(result, msgOut);
         _floor_pub.publish(msgOut);
-        sensor_msgs::PointCloud2 allOut;
-        input->header.frame_id = "floor";
-        pcl::toROSMsg(*input, allOut);
-        _all_pub.publish(allOut);
     }
 
 private:
     ros::NodeHandle _nh;
     ros::Subscriber _pcl_sub;
     ros::Publisher _floor_pub;
-    ros::Publisher _all_pub;
     tf::TransformBroadcaster _tb;
     tf::Transform transform;
     tf::Quaternion q;
-
 };
 
 int main(int argc, char** argv) {
